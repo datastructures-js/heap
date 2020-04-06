4c606f85c1a4104665bb8afc9b37dcdac1620373
Eyas Ranjous <eyas.ranjous@gmail.com>
2020-04-05 20:50:14 -0500

    jsdoc

--- src/maxHeap.js
+++ src/maxHeap.js
@@ -1,6 +1,6 @@
 /**
- * datastructures-js/heap
- * @copyright 2019 Eyas Ranjous <eyas.ranjous@gmail.com>
+ * @datastructures-js/heap
+ * @copyright 2020 Eyas Ranjous <eyas.ranjous@gmail.com>
  * @license MIT
  */
 
@@ -12,40 +12,15 @@ const Heap = require('./heap');
  */
 class MaxHeap extends Heap {
   /**
-   * inserts a node into the heap and rebase leaf node to min key
-   * @param {number|string} key
-   * @param {object} value
-   * @public
-   * @override
-   */
-  insert(key, value) {
-    const newNode = super.insert(key, value);
-    if (this.leafNode === null || key < this.leafNode.getKey()) {
-      this.leafNode = newNode;
-    }
-  }
-
-  /**
-   * checks if child's key is bigger that its parent's key
-   * @protected
-   * @returns {boolean}
-   */
-  shouldSwap(childIndex, parentIndex) {
-    const child = this.nodes[childIndex];
-    const parent = this.nodes[parentIndex];
-    return child.getKey() > parent.getKey();
-  }
-
-  /**
-   * gets the max child's index of two node's children
    * @private
+   * gets the max child's index of two node's children
    * @param {number} leftChildIndex
    * @param {number} rightChildIndex
    * @returns {number}
    */
-  getMaxChildIndex(leftChildIndex, rightChildIndex) {
-    const leftChild = this.nodes[leftChildIndex];
-    const rightChild = this.nodes[rightChildIndex];
+  _getMaxChildIndex(leftChildIndex, rightChildIndex) {
+    const leftChild = this._nodes[leftChildIndex];
+    const rightChild = this._nodes[rightChildIndex];
     if (leftChild.getKey() > rightChild.getKey()) {
       return leftChildIndex;
     }
@@ -53,16 +28,16 @@ class MaxHeap extends Heap {
   }
 
   /**
-   * returns the max child's index of two children before an index
    * @private
+   * returns the max child's index of two children before an index
    * @param {number} index
    * @param {number} leftChildIndex
    * @param {number} rightChildIndex
    * @returns {number}
    */
-  getMaxChildIndexBefore(index, leftChildIndex, rightChildIndex) {
-    const leftChild = this.nodes[leftChildIndex];
-    const rightChild = this.nodes[rightChildIndex];
+  _getMaxChildIndexBefore(index, leftChildIndex, rightChildIndex) {
+    const leftChild = this._nodes[leftChildIndex];
+    const rightChild = this._nodes[rightChildIndex];
     if (rightChild.getKey() > leftChild.getKey() && rightChildIndex < index) {
       return rightChildIndex;
     }
@@ -70,50 +45,74 @@ class MaxHeap extends Heap {
   }
 
   /**
-   * implements the parent's function to select a child's index
+   * @protected
+   * checks if child's key is bigger that its parent's key
+   * @returns {boolean}
+   */
+  _shouldSwap(childIndex, parentIndex) {
+    const child = this._nodes[childIndex];
+    const parent = this._nodes[parentIndex];
+    return child.getKey() > parent.getKey();
+  }
+
+  /**
    * @protected
    * @override
+   * implements the parent's function to select a child's index
    * @param {number} leftChildIndex
    * @param {number} rightChildIndex
    * @returns {number}
    */
-  compareChildren(leftChildIndex, rightChildIndex) {
-    return this.getMaxChildIndex(leftChildIndex, rightChildIndex);
+  _compareChildren(leftChildIndex, rightChildIndex) {
+    return this._getMaxChildIndex(leftChildIndex, rightChildIndex);
   }
 
   /**
-   * implements the parent's function to select a child's index before an index
    * @protected
    * @override
+   * implements the parent's function to select a child's index before an index
    * @param {number} index
    * @param {number} leftChildIndex
    * @param {number} rightChildIndex
    * @returns {number}
    */
-  compareChildrenBefore(index, leftChildIndex, rightChildIndex) {
-    return this.getMaxChildIndexBefore(index, leftChildIndex, rightChildIndex);
+  _compareChildrenBefore(index, leftChildIndex, rightChildIndex) {
+    return this._getMaxChildIndexBefore(index, leftChildIndex, rightChildIndex);
   }
 
   /**
-   * returns a shallow copy of a max heap
    * @public
    * @override
+   * inserts a node into the heap and rebase leaf node to min key
+   * @param {number|string} key
+   * @param {object} value
+   */
+  insert(key, value) {
+    const newNode = super.insert(key, value);
+    if (this._leaf === null || key < this._leaf.getKey()) {
+      this._leaf = newNode;
+    }
+    return newNode;
+  }
+
+  /**
+   * @public
+   * returns a shallow copy of a max heap
    * @returns {MaxHeap}
    */
   clone() {
-    return super.clone(MaxHeap);
+    return super._clone(MaxHeap);
   }
 
   /**
+   * @public
+   * @static
    * builds a max heap from an array of items
    * @param {array} items
-   * @public
-   * @override
-   * @static
    * @returns {MaxHeap}
    */
   static heapify(items) {
-    return super.heapify(items, MaxHeap);
+    return super._heapify(items, MaxHeap);
   }
 }
 
