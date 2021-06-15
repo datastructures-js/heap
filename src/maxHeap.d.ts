565abb20386de6a2858acc5c601e44b09219f95e
Eyas Ranjous <eyas.ranjous@gmail.com>
2021-06-15 09:38:47 +0200

    typo

--- src/maxHeap.d.ts
+++ src/maxHeap.d.ts
@@ -1,6 +1,6 @@
 import { HeapNode, Heap } from './heap';
 
-export abstract class MaxHeap<T extends number|string, U> extends Heap<T, U> {
+export class MaxHeap<T extends number|string, U> extends Heap<T, U> {
   clone(): MaxHeap<T, U>;
   static heapify<T extends number|string, U>(list: (HeapNode<T, U> | T)[]): MaxHeap<T, U>;
   static isHeapified<T extends number|string, U>(list: (HeapNode<T, U> | T)[]): boolean;
