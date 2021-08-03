25103c5c6b8ee07402126cc130b82ac77cf25193
Eyas Ranjous <eyas.ranjous@gmail.com>
2021-08-03 03:01:01 +0200

    add CustomHeap

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const { MinHeap } = require('./src/minHeap');
 const { MaxHeap } = require('./src/maxHeap');
+const { CustomHeap } = require('./src/customHeap');
 
 exports.MinHeap = MinHeap;
 exports.MaxHeap = MaxHeap;
+exports.CustomHeap = CustomHeap;
