7324c6be4ed535bb4d04e85c81148cb100ee68c4
masx200 <34191203+masx200@users.noreply.github.com>
2022-12-24 19:52:03 +0800

    Update minHeap.d.ts

--- src/minHeap.d.ts
+++ src/minHeap.d.ts
@@ -2,28 +2,28 @@ import { Heap } from "./heap";
 import { IGetCompareValue } from "./maxHeap";
 
 export class MinHeap<T> implements Iterable<T> {
-    constructor(getCompareValue?: IGetCompareValue<T>, _heap?: Heap<T>);
-    [Symbol.iterator](): Iterator<T, any, undefined>;
-    insert(value: T): MinHeap<T>;
-    push(value: T): MinHeap<T>;
-    extractRoot(): T;
-    pop(): T;
-    sort(): T[];
-    fix(): MinHeap<T>;
-    isValid(): boolean;
-    clone(): MinHeap<T>;
-    root(): T;
-    top(): T;
-    leaf(): T;
-    size(): number;
-    isEmpty(): boolean;
-    clear(): void;
-    static heapify<T>(
-        values: T[],
-        getCompareValue?: IGetCompareValue<T>
-    ): MinHeap<T>;
-    static isHeapified<T>(
-        values: T[],
-        getCompareValue?: IGetCompareValue<T>
-    ): boolean;
+  constructor(getCompareValue?: IGetCompareValue<T>, _heap?: Heap<T>);
+  [Symbol.iterator](): Iterator<T, any, undefined>;
+  insert(value: T): MinHeap<T>;
+  push(value: T): MinHeap<T>;
+  extractRoot(): T;
+  pop(): T;
+  sort(): T[];
+  fix(): MinHeap<T>;
+  isValid(): boolean;
+  clone(): MinHeap<T>;
+  root(): T;
+  top(): T;
+  leaf(): T;
+  size(): number;
+  isEmpty(): boolean;
+  clear(): void;
+  static heapify<T>(
+    values: T[],
+    getCompareValue?: IGetCompareValue<T>
+  ): MinHeap<T>;
+  static isHeapified<T>(
+    values: T[],
+    getCompareValue?: IGetCompareValue<T>
+  ): boolean;
 }
