9c462f4e8d9b5582a2af1d4210d29e868c72def9
masx200 <34191203+masx200@users.noreply.github.com>
2022-12-24 19:51:43 +0800

    Update maxHeap.d.ts

--- src/maxHeap.d.ts
+++ src/maxHeap.d.ts
@@ -1,32 +1,32 @@
 import { Heap } from "./heap";
 
 export interface IGetCompareValue<T> {
-    (value: T): number | string;
+  (value: T): number | string;
 }
 
 export class MaxHeap<T> implements Iterable<T> {
-    constructor(getCompareValue?: IGetCompareValue<T>, _heap?: Heap<T>);
-    [Symbol.iterator](): Iterator<T, any, undefined>;
-    insert(value: T): MaxHeap<T>;
-    push(value: T): MaxHeap<T>;
-    extractRoot(): T;
-    pop(): T;
-    sort(): T[];
-    fix(): MaxHeap<T>;
-    isValid(): boolean;
-    clone(): MaxHeap<T>;
-    root(): T;
-    top(): T;
-    leaf(): T;
-    size(): number;
-    isEmpty(): boolean;
-    clear(): void;
-    static heapify<T>(
-        values: T[],
-        getCompareValue?: IGetCompareValue<T>
-    ): MaxHeap<T>;
-    static isHeapified<T>(
-        values: T[],
-        getCompareValue?: IGetCompareValue<T>
-    ): boolean;
+  constructor(getCompareValue?: IGetCompareValue<T>, _heap?: Heap<T>);
+  [Symbol.iterator](): Iterator<T, any, undefined>;
+  insert(value: T): MaxHeap<T>;
+  push(value: T): MaxHeap<T>;
+  extractRoot(): T;
+  pop(): T;
+  sort(): T[];
+  fix(): MaxHeap<T>;
+  isValid(): boolean;
+  clone(): MaxHeap<T>;
+  root(): T;
+  top(): T;
+  leaf(): T;
+  size(): number;
+  isEmpty(): boolean;
+  clear(): void;
+  static heapify<T>(
+    values: T[],
+    getCompareValue?: IGetCompareValue<T>
+  ): MaxHeap<T>;
+  static isHeapified<T>(
+    values: T[],
+    getCompareValue?: IGetCompareValue<T>
+  ): boolean;
 }
