a88ec6c6d5575c85a591b4f733f430709f2f7a6d
masx200 <34191203+masx200@users.noreply.github.com>
2022-12-24 19:51:07 +0800

    Update heap.d.ts

--- src/heap.d.ts
+++ src/heap.d.ts
@@ -1,24 +1,24 @@
 export interface ICompare<T> {
-    (a: T, b: T): number;
+  (a: T, b: T): number;
 }
 
 export class Heap<T> implements Iterable<T> {
-    constructor(comparator: ICompare<T>, values?: T[], leaf?: T);
-    [Symbol.iterator](): Iterator<T, any, undefined>;
-    insert(value: T): Heap<T>;
-    push(value: T): Heap<T>;
-    extractRoot(): T;
-    pop(): T;
-    sort(): T[];
-    fix(): Heap<T>;
-    isValid(): boolean;
-    clone(): Heap<T>;
-    root(): T;
-    top(): T;
-    leaf(): T;
-    size(): number;
-    isEmpty(): boolean;
-    clear(): void;
-    static heapify<T>(values: T[], comparator: ICompare<T>): Heap<T>;
-    static isHeapified<T>(values: T[], comparator: ICompare<T>): boolean;
+  constructor(comparator: ICompare<T>, values?: T[], leaf?: T);
+  [Symbol.iterator](): Iterator<T, any, undefined>;
+  insert(value: T): Heap<T>;
+  push(value: T): Heap<T>;
+  extractRoot(): T;
+  pop(): T;
+  sort(): T[];
+  fix(): Heap<T>;
+  isValid(): boolean;
+  clone(): Heap<T>;
+  root(): T;
+  top(): T;
+  leaf(): T;
+  size(): number;
+  isEmpty(): boolean;
+  clear(): void;
+  static heapify<T>(values: T[], comparator: ICompare<T>): Heap<T>;
+  static isHeapified<T>(values: T[], comparator: ICompare<T>): boolean;
 }
