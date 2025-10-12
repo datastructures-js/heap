807a252d521f14691782e8e8dae55913d97199e4
eyas-ranjous <eyas.ranjous@gmail.com>
2025-10-11 18:41:54 -0700

    fix ts types

--- src/maxHeap.d.ts
+++ src/maxHeap.d.ts
@@ -4,24 +4,12 @@ export interface IGetCompareValue<T> {
   (value: T): number | string;
 }
 
-export class MaxHeap<T> implements Iterable<T> {
-  constructor(getCompareValue?: IGetCompareValue<T>, _heap?: Heap<T>);
-  toArray(): T[];
-  [Symbol.iterator](): Iterator<T, any, undefined>;
+export class MaxHeap<T> extends Heap<T> {
+  constructor(getCompareValue?: IGetCompareValue<T>, values?: T[]);
   insert(value: T): MaxHeap<T>;
   push(value: T): MaxHeap<T>;
-  extractRoot(): T | null;
-  pop(): T | null;
-  sort(): T[];
   fix(): MaxHeap<T>;
-  isValid(): boolean;
   clone(): MaxHeap<T>;
-  root(): T | null;
-  top(): T | null;
-  leaf(): T | null;
-  size(): number;
-  isEmpty(): boolean;
-  clear(): void;
   static heapify<T>(
     values: T[],
     getCompareValue?: IGetCompareValue<T>
