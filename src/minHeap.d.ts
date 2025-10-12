45b953d3fb200e0be602d6d150c89f954f63dbe2
eyas <eyas.ranjous@gmail.com>
2025-10-11 18:43:30 -0700

    fix ts types

--- src/minHeap.d.ts
+++ src/minHeap.d.ts
@@ -1,24 +1,12 @@
 import { Heap } from "./heap";
 import { IGetCompareValue } from "./maxHeap";
 
-export class MinHeap<T> implements Iterable<T> {
-  constructor(getCompareValue?: IGetCompareValue<T>, _heap?: Heap<T>);
-  toArray(): T[];
-  [Symbol.iterator](): Iterator<T, any, undefined>;
+export class MinHeap<T> extends Heap<T> {
+  constructor(getCompareValue?: IGetCompareValue<T>, values?: T[]);
   insert(value: T): MinHeap<T>;
   push(value: T): MinHeap<T>;
-  extractRoot(): T | null;
-  pop(): T | null;
-  sort(): T[];
   fix(): MinHeap<T>;
-  isValid(): boolean;
   clone(): MinHeap<T>;
-  root(): T | null;
-  top(): T | null;
-  leaf(): T | null;
-  size(): number;
-  isEmpty(): boolean;
-  clear(): void;
   static heapify<T>(
     values: T[],
     getCompareValue?: IGetCompareValue<T>
